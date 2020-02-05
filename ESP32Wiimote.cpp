2f13d7e9f9a2cff2242711344560f0df438b9f2d
daiki <daiking_tensai@yahoo.co.jp>
2020-02-05 10:05:08 +0900

    divide ESP32 specific code

--- ESP32Wiimote.cpp
+++ ESP32Wiimote.cpp
@@ -12,23 +12,174 @@
 //   Under the following terms:
 //     NonCommercial — You may not use the material for commercial purposes.
 
+#define CONFIG_CLASSIC_BT_ENABLED
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "Arduino.h"
+#include "esp_bt.h"
+#include <HardwareSerial.h>
+
+#include "time.h"
+#include "sys/time.h"
+
 #include "ESP32Wiimote.h"
 #include "TinyWiimote.h"
+
+#define WIIMOTE_VERBOSE 0
+
+#if WIIMOTE_VERBOSE
+#define VERBOSE_PRINT(...) Serial.printf(__VA_ARGS__)
+#define VERBOSE_PRINTLN(...) Serial.println(__VA_ARGS__)
+#else
+#define VERBOSE_PRINT(...) do {} while(0)
+#define VERBOSE_PRINTLN(...) do {} while(0)
+#endif
 
 ESP32Wiimote::ESP32Wiimote(void)
 {
     ;
 }
 
+void notifyHostSendAvailable(void) {
+  VERBOSE_PRINT("notifyHostSendAvailable\n");
+  if(!TinyWiimoteDeviceIsInited()){
+    TinyWiimoteResetDevice();
+  }
+}
+
+typedef struct {
+        size_t len;
+        uint8_t data[];
+} queuedata_t;
+#define RX_QUEUE_SIZE 32
+#define TX_QUEUE_SIZE 32
+xQueueHandle rxQueue = NULL;
+xQueueHandle txQueue = NULL;
+
+void createQueue() {
+  txQueue = xQueueCreate(TX_QUEUE_SIZE, sizeof(queuedata_t*));
+  if (txQueue == NULL){
+    VERBOSE_PRINTLN("xQueueCreate(txQueue) failed");
+    return;
+  }
+  rxQueue = xQueueCreate(RX_QUEUE_SIZE, sizeof(queuedata_t*));
+  if (rxQueue == NULL){
+    VERBOSE_PRINTLN("xQueueCreate(rxQueue) failed");
+    return;
+  }
+}
+
+void handleTxQueue() {
+  if(uxQueueMessagesWaiting(txQueue)){
+    bool ok = esp_vhci_host_check_send_available();
+    VERBOSE_PRINT("esp_vhci_host_check_send_available=%d", ok);
+    if(ok){
+      queuedata_t *queuedata = NULL;
+      if(xQueueReceive(txQueue, &queuedata, 0) == pdTRUE){
+        esp_vhci_host_send_packet(queuedata->data, queuedata->len);
+        VERBOSE_PRINT("SEND => %s", format2Hex(queuedata->data, queuedata->len));
+        free(queuedata);
+      }
+    }
+  }
+}
+
+void handleRxQueue() {
+  if(uxQueueMessagesWaiting(rxQueue)){
+    queuedata_t *queuedata = NULL;
+    if(xQueueReceive(rxQueue, &queuedata, 0) == pdTRUE){
+      handleHciData(queuedata->data, queuedata->len);
+      free(queuedata);
+    }
+  }
+}
+
+static esp_err_t _sendQueueData(xQueueHandle queue, uint8_t *data, size_t len) {
+    VERBOSE_PRINTLN("_sendQueueData");
+    if(!data || !len){
+        VERBOSE_PRINTLN("no data");
+        return ESP_OK;
+    }
+    queuedata_t * queuedata = (queuedata_t*)malloc(sizeof(queuedata_t) + len);
+    if(!queuedata){
+        VERBOSE_PRINTLN("malloc failed");
+        return ESP_FAIL;
+    }
+    queuedata->len = len;
+    memcpy(queuedata->data, data, len);
+    if (xQueueSend(queue, &queuedata, portMAX_DELAY) != pdPASS) {
+        VERBOSE_PRINTLN("xQueueSend failed");
+        free(queuedata);
+        return ESP_FAIL;
+    }
+    return ESP_OK;
+}
+
+void hciHostSendPacket(uint8_t *data, size_t len) {
+  _sendQueueData(txQueue, data, len);
+}
+
+void hciHostRecvPacket(uint8_t *data, size_t len) {
+  _sendQueueData(rxQueue, data, len);
+}
+
+
+int notifyHostRecv(uint8_t *data, uint16_t len) {
+  VERBOSE_PRINT("notifyHostRecv:");
+  for (int i = 0; i < len; i++)
+  {
+    VERBOSE_PRINT(" %02x", data[i]);
+  }
+  VERBOSE_PRINTLN("");
+
+  if(ESP_OK == _sendQueueData(rxQueue, data, len)){
+    return ESP_OK;
+  }else{
+    return ESP_FAIL;
+  }
+}
+
+static const TwHciInterface tinywii_hci_interface = {
+  hciHostSendPacket,
+  hciHostRecvPacket
+};
+
+esp_vhci_host_callback_t vhci_callback;
+
 void ESP32Wiimote::init(void)
 {
-    TinyWiimoteInit();
+    TinyWiimoteInit(tinywii_hci_interface);
+    createQueue();
+    vhci_callback.notify_host_recv = notifyHostRecv;
+    vhci_callback.notify_host_send_available = notifyHostSendAvailable;    
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    if (!btStart()) {
+        Serial.printf( "btStart() failed\n");
+        return;
+    }
+
+    esp_err_t ret;
+    if ((ret = esp_vhci_host_register_callback(&vhci_callback)) != ESP_OK) {
+        return;
+    }
+
 }
 
 void ESP32Wiimote::task(void)
 {
-  TinyWiimoteHandle();
+  if(!btStarted()){
+    return;
+  }
+  handleTxQueue();
+  handleRxQueue();
 }
 
 int ESP32Wiimote::available(void)
@@ -39,8 +190,10 @@ int ESP32Wiimote::available(void)
 uint16_t ESP32Wiimote::getButtonState(void)
 {
   uint16_t button = 0;
-  twii_recv_data_st getData = TinyWiimoteRead();
+  TinyWiimoteData getData = TinyWiimoteRead();
   button = (getData.data[TWII_OFFSET_BTNS1] << 8) | getData.data[TWII_OFFSET_BTNS2];
   return button;
 }
+
+
 
--- TinyWiimote.h
+++ TinyWiimote.h
@@ -16,7 +16,7 @@
 #define _TINY_WIIMOTE_H_
 
 #define RECIEVED_DATA_MAX_LEN     (50)
-struct twii_recv_data_st {
+struct TinyWiimoteData {
   uint8_t number;
   uint8_t data[RECIEVED_DATA_MAX_LEN];
   uint8_t len;
@@ -24,10 +24,33 @@ struct twii_recv_data_st {
 #define TWII_OFFSET_BTNS1 (2)
 #define TWII_OFFSET_BTNS2 (3)
 
-void TinyWiimoteInit();
-void TinyWiimoteHandle();
-int TinyWiimoteAvailable(void);
-twii_recv_data_st TinyWiimoteRead(void);
+typedef struct tinywii_device_callback {
+    void (*hci_send_packet)(uint8_t *data, size_t len);
+    void (*hci_recv_packet)(uint8_t *data, size_t len);
+} TwHciInterface;
 
+// typedef struct {
+//         size_t len;
+//         uint8_t data[];
+// } queuedata_t;
+// #define RX_QUEUE_SIZE 32
+// #define TX_QUEUE_SIZE 32
+// extern xQueueHandle rxQueue;
+// extern xQueueHandle txQueue;
+
+void TinyWiimoteInit(TwHciInterface hciInterface);
+int TinyWiimoteAvailable(void);
+TinyWiimoteData TinyWiimoteRead(void);
+
+void TinyWiimoteResetDevice(void);
+bool TinyWiimoteDeviceIsInited(void);
+void handleHciEvent(uint8_t event_code, uint8_t len, uint8_t* data);
+void handleAclData(uint8_t* data, size_t len);
+void handleHciData(uint8_t* data, size_t len);
+
+// void handleRxQueue(void);
+// void handleTxQueue(void);
+
+char* format2Hex(uint8_t* data, uint16_t len);
 
 #endif // _TINY_WIIMOTE_H_
